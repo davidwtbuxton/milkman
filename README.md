9d6c4c28c5fced11d16cf2d06a712968dfd19ad8
David Buxton <david@gasmark6.com>
2014-02-11 10:00:50 +0000

    Fixes pip install (#30).
    - Simplify setup.py and change license name to MIT.
    - Adds docs/index.txt to distribution, removes .egg-info package.
    - Adds support for running tests with tox.
    - Fixes test settings.py so tests can run (but still lots of failures).
    - Replace PIL with Pillow in requirements.txt.

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Quick Examples
 
 	from milkman.dairy import milkman
 	from myapp.models import Video, Editor
-	
+
 	#I just want a random video in the db to test with
 	v = milkman.deliver(Video)
-	
+
 	#I want a video with a particular editor
 	e = milkman.deliver(Editor)
 	v = milkman.deliver(Video, editor=e)
-	
-	
+
+
 Development
 -----------
 Install virtualenv and virtualenvwrapper
@@ -32,7 +32,11 @@ Running Tests
 
     cd testapp
     ./manage.py test
-  
+
+In addition you can install [the tox testing tool](http://tox.testrun.org/) to quickly run the tests against multiple installed versions of Python and Django.
+
+Once tox has been installed you can run tests from the source directory with `tox`. This will build virtual environments for the versions defined in the file tox.ini and then install and test the source in each.
+
 
 Known Issues
 ------------
--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 
 milkman = __import__("milkman")
-readme_file = os.path.join(os.path.dirname(__file__), "docs/index.txt")
-try:
-    long_description = open(readme_file).read()
-except IOError as err:
-    sys.stderr.write("[ERROR] Cannot find file specified as "
-        "``long_description`` (%s)\n" % readme_file)
-    sys.exit(1)
+readme_file = os.path.join(os.path.dirname(__file__), 'docs', 'index.txt')
+long_description = open(readme_file).read()
 
 if sys.version_info >= (3,):
     extra_kwargs = {"use_2to3": True}
@@ -23,11 +18,10 @@ setup(
     long_description=long_description,
     author="Wilkes Joiner, Chuck Collins, Patrick Altman",
     author_email="chuck.collins@gmail.com",
-    license="BSD",
+    license="MIT",
     url="http://github.com/ccollins/milkman/",
     keywords="django testing mock stub",
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    include_package_data=True,
+    packages=['milkman'],
     install_requires=[
         "docutils>=0.3",
     ],
